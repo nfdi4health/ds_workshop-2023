de8bee3a0edce4279912b465591ac9754ef7153d
Sofia Siampani <sofiamaria.siampani@mdc-berlin.de>
2023-09-07 16:10:34 +0200

    added more functions

--- ds-starter.R
+++ ds-starter.R
@@ -10,12 +10,33 @@ builder$append(server="server1", url="https://dsmolep.mdc-berlin.de",
                table = "Test.harmonized_study_TEST_table_TEST")
 logindata <- builder$build()
 
+
 # Then perform login in each server
 library(DSOpal)
 connections <- datashield.login(logins=logindata, assign = T)
 
-ds.colnames("D")
+# Administrative functions
+ds.listClientsideFunctions()
+ds.listServersideFunctions()
+ds.listDisclosureSettings()
+ds.listOpals()
 
+
+# Data Analysis functions (generally of type "Aggregate")
+## Data structure queries
+ds.class("D$AGE")
+
+ds.class("D$SEX")
+ds.levels("D$SEX")
+
+ds.class("D$SMOKE_ST")
+ds.levels("D$SEX")
+
+ds.class("D$SOD_POT")
+
+## Summary Statistics Functions
 ds.summary("D$AGE")
-
 ds.summary("D$SEX")
+ds.summary("D$SMOKE_ST")
+ds.summary("D$SOD_POT") # we can show how this can be automated with datashieldDescriptives
+
